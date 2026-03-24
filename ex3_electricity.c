38073045a4eac4ecd7ade18dbe6b0e64f3df0d84
MALIMAUREEN <amalimoureen85@gmail.com>
2026-03-24 20:36:59 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,18 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: AMALI MAUREEN LYDIA*/
+/* Student Number: 25/U/BIE/05286/PE*/
+#include <stdio.h>0
+int main() {
+    float units_consumed, cost_per_unit, total_bill;
 
-#include <stdio.h>
+    printf("Enter units_consumed");
+    scanf("%f", &units_consumed);
 
-int main(void)
-{
-    /* Write your code here */
+    printf("Enter cost_per_unit");
+    scanf("%f", &cost_per_unit);
+
+    total_bill = units_consumed*cost_per_unit;
+    
+    printf("total_bill: %.2f", total_bill);
 
     return 0;
 }
